acdd0b3e0edb995d1890f11450085cc36ed12960
Geovanni Morales <Geovanni@PocketWatch.local>
2019-03-29 08:23:58 -0700

    Experimenting with Modals

--- Screens/SelectionScreen.js
+++ Screens/SelectionScreen.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {Modal, Text, TouchableHighlight, View, Alert, Button, StyleSheet} from 'react-native';
 
 export default class SelectionScreen extends React.Component {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>SelectionScreen</Text>
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+          }}>
+          <View style={{marginTop: 22}}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Button
+                onPress={() => {this.setModalVisible(false)
+                }}
+                title="Close"
+                color="#0000ff"
+                
+/>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight 
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+                <Button
+                onPress={() => {this.setModalVisible(true)
+                }}
+                title="Open"
+                color="#0000ff"
+                
+/>
+        </TouchableHighlight>
       </View>
     );
   }
