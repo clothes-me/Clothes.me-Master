aa1c374e6bf0cf45f30662d23ff7ef956b901c6e
Geovanni Morales <Geovanni@PocketWatch.local>
2019-05-10 07:51:21 -0700

    HomeScreen Complete

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
+import { Font } from 'expo';
 
 export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.buttonTrigger = this.buttonTrigger.bind(this);
+    
+    this.state = {
+      fontLoaded: false,
+    };
+}
+  async componentDidMount() {
+    await Font.loadAsync({
+      'AbrilFatface-Regular': require('../assets/fonts/AbrilFatface-Regular.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  buttonTrigger() {
+    this.props.navigation.navigate('Selection')
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>HomeScreen</Text>
+       {
+          this.state.fontLoaded ? (
+          <Text style = {styles.headingText}>Clothes Encounters</Text>
+          ): null }
+          <Text style = {styles.loweredText}>Make Clothes Easy</Text>
+          
+          
+          <Image
+          style={{width: 350, height: 350,flex:1}}
+          source={require('../assets/Shirt.png')}/>
+          
+
+          <TouchableHighlight 
+                style={styles.button}
+                onPress={this.buttonTrigger}
+                underlayColor = "white">
+
+              <Text style = {{color: 'white'}}> Start Now </Text>
+
+          </TouchableHighlight>
+
+
       </View>
     );
   }
@@ -14,8 +57,36 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  heading: {
+    flex: 2,
+    alignItems: 'center',
+  },
+  headingText: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 60,
+    margin: 22,
+    marginBottom: 0,
+    fontFamily: 'AbrilFatface-Regular',
+    textAlign: 'center',
+  },
+
+  loweredText: {
+    fontFamily: 'Palatino',
+    textAlign: 'left',
+  },
+  button: {
+		backgroundColor: 'black',
+    borderWidth: 0,
+    borderColor: '#eee',
+    borderStyle: 'solid',
+    borderRadius: 4,
+    overflow: 'hidden',
+    padding: 10,
+    margin: 10
+	}
 });
--- Screens/ResultsScreen.js
+++ Screens/ResultsScreen.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
 
 export default class ResultsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.buttonTrigger = this.buttonTrigger.bind(this);
+    
+}
+  
+  buttonTrigger() {
+    this.props.navigation.navigate('Selection')
+  }
+  
   render() {
     return (
       <View style={styles.container}>
+        <View style = {styles.image}>
+          <Image
+              style={{width:250, height:250}} 
+              source={require('../assets/1Star.png') }/>
+          <Image
+              style={{width:250, height:250}} 
+              source={require('../assets/5Star.png') }/>
+          
+        </View>
         <Text>ResultsScreen</Text>
+        <TouchableHighlight 
+                style={styles.button}
+                onPress={this.buttonTrigger}
+                underlayColor = "white">
+
+              <Text style={styles.buttonText}> Start Now </Text>
+
+          </TouchableHighlight>
       </View>
     );
   }
@@ -14,8 +42,25 @@ export default class ResultsScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    justifyContent: 'flex-end',
   },
+  image: {
+    flex:1,
+    alignItems: 'center',
+  },
+  button: {
+		backgroundColor: 'black',
+    borderWidth: 0,
+    borderColor: '#eee',
+    borderStyle: 'solid',
+    borderRadius: 4,
+    overflow: 'hidden',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  }
 });
